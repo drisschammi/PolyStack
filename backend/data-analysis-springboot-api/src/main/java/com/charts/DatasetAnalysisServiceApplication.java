8f0b8a77589ba0f4da35dabbf8b3951b8c185ee1
driss-chammi <driss.chammi@usmba.ac.ma>
2025-01-29 20:21:37 +0100

    Initial commit: Create Spring Boot application with Gradle Kotlin DSL

---
+++ backend/data-analysis-springboot-api/src/main/java/com/charts/DatasetAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.charts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetAnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ backend/data-analysis-springboot-api/src/test/java/com/charts/DatasetAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
