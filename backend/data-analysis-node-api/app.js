418060808f20870a2e78e8052cbbd48cb9b4fdfb
driss-chammi <driss.chammi@usmba.ac.ma>
2025-01-30 14:31:59 +0100

    Add Swagger annotations for API documentation

--- backend/data-analysis-node-api/app.js
+++ backend/data-analysis-node-api/app.js
@@ -2,12 +2,43 @@ import cors from "cors";
 import express from 'express';
 import coverageRoutes from './src/routes/analysisRoutes.js';
 import { errorHandler, notFound } from "./src/middleware/errorHandler.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Swagger JSDoc configuration
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Insurance Policy Analysis API",
+            version: "1.0.0",  // API version
+            description: "API documentation for insurance policy analysis",  // API description
+            contact: {
+                name: "Chammi Driss",  // Contact name
+                email: "drisschami24@gmail.com",  // Contact email
+                url: "https://ma.linkedin.com/in/drisschammi"  // Contact URL
+            }
+        },
+        servers: [
+            {
+                url: `http://localhost:${process.env.PORT || 3000}/`,  // Your server URL
+            },
+        ],
+    },
+    apis: ["./src/routes/*.js"],  // Path to your route files
+};
+
+// Generate Swagger specification
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+// Swagger documentation
+app.use("/swagger-ui", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 app.use('/api/v1', coverageRoutes);
--- backend/data-analysis-node-api/src/routes/analysisRoutes.js
+++ backend/data-analysis-node-api/src/routes/analysisRoutes.js
@@ -4,11 +4,73 @@ import asyncHandler from "../middleware/asyncHandler.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/coverage/vehicle-segment:
+ *   get:
+ *     summary: Get coverage data by vehicle segment
+ *     description: Retrieves insurance coverage data by vehicle segment.
+ *     responses:
+ *       200:
+ *         description: Successful response with coverage data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   vehicle_segment:
+ *                     type: string
+ *                   bodily_injury_liability:
+ *                     type: integer
+ *                   personal_injury_protection:
+ *                     type: integer
+ *                   property_damage_liability:
+ *                     type: integer
+ *                   collision:
+ *                     type: integer
+ *                   comprehensive:
+ *                     type: integer
+ *       500:
+ *         description: Internal server error
+ */
 router.get(
     "/coverage/vehicle-segment",
     asyncHandler(getCoverageByVehicleSegment)
 );
 
+/**
+ * @swagger
+ * /api/v1/coverage/fuel-type:
+ *   get:
+ *     summary: Get coverage data by fuel type
+ *     description: Retrieves insurance coverage data based on fuel type.
+ *     responses:
+ *       200:
+ *         description: Successful response with coverage data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   fuel:
+ *                     type: string
+ *                   bodily_injury_liability:
+ *                     type: integer
+ *                   personal_injury_protection:
+ *                     type: integer
+ *                   property_damage_liability:
+ *                     type: integer
+ *                   collision:
+ *                     type: integer
+ *                   comprehensive:
+ *                     type: integer
+ *       500:
+ *         description: Internal server error
+ */
 router.get(
     "/coverage/fuel-type",
     asyncHandler(getCoverageByFuelType)
